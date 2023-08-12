6796dc7340cec4f727d1f8ad34a329860a563e14
joneswang <jonescwang@gmail.com>
2023-08-12 13:01:31 -0400

    spin wheel is available in the screen

--- app/src/main/java/com/jonesandjay123/wheelofchoices/MainActivity.kt
+++ app/src/main/java/com/jonesandjay123/wheelofchoices/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -16,6 +17,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jonesandjay123.wheelofchoices.databinding.ActivityMainBinding
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
 class MainActivity : Activity() {
 
@@ -26,6 +29,10 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 使用 binding 訪問 WheelView
+        val wheelView = binding.wheelView
+        // 你可以在這裡設置 WheelView 的一些屬性，例如設置選項等
 
         // 這些是你的選項
         val options = listOf("Option 1", "Option 2", "Option 3")
@@ -58,7 +65,7 @@ class OptionsAdapter(private val options: List<String>) :
 class WheelView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     // 轉盤的半徑
-    private val wheelRadius = 100f
+    private val wheelRadius = 135f
 
     // 指針的半徑
     private val pointerRadius = 10f
@@ -74,6 +81,9 @@ class WheelView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     // 轉盤的當前角度
     private var currentAngle = 0f
+
+    private var previousX: Float = 0f
+    private var previousY: Float = 0f
 
     // 繪製轉盤的畫筆
     private val wheelPaint = Paint().apply {
@@ -126,8 +136,33 @@ class WheelView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        // 在這裡我們將處理轉盤的觸摸事件
-        return super.onTouchEvent(event)
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                previousX = x
+                previousY = y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val dx = x - previousX
+                val dy = y - previousY
+
+                // 根據移動的距離計算角度
+                val distanceFromCenter = sqrt(dx * dx + dy * dy)
+                val angle = Math.toDegrees(atan2(dy.toDouble(), dx.toDouble())).toFloat()
+
+                // 更新當前的角度
+                currentAngle += angle * distanceFromCenter / wheelRadius
+
+                invalidate() // 請求重畫視圖
+
+                previousX = x
+                previousY = y
+            }
+        }
+
+        return true
     }
 }
 
