12b3f59d6b4d2019ff578107466bfe6b09bac3cc
joneswang <jonescwang@gmail.com>
2023-08-12 11:19:07 -0400

    update mainactivity by adding wheelview

--- app/src/main/java/com/jonesandjay123/wheelofchoices/MainActivity.kt
+++ app/src/main/java/com/jonesandjay123/wheelofchoices/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.jonesandjay123.wheelofchoices
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -47,3 +54,80 @@ class OptionsAdapter(private val options: List<String>) :
 
     override fun getItemCount() = options.size
 }
+
+class WheelView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    // 轉盤的半徑
+    private val wheelRadius = 100f
+
+    // 指針的半徑
+    private val pointerRadius = 10f
+
+    // 轉盤的顏色
+    private val wheelColor = Color.parseColor("#E91E63")
+
+    // 指針的顏色
+    private val pointerColor = Color.parseColor("#FFC107")
+
+    // 轉盤上的選項
+    private val options = arrayOf("Option 1", "Option 2", "Option 3", "Option 4")
+
+    // 轉盤的當前角度
+    private var currentAngle = 0f
+
+    // 繪製轉盤的畫筆
+    private val wheelPaint = Paint().apply {
+        color = wheelColor
+        style = Paint.Style.FILL
+        isAntiAlias = true
+    }
+
+    // 繪製指針的畫筆
+    private val pointerPaint = Paint().apply {
+        color = pointerColor
+        style = Paint.Style.FILL
+        isAntiAlias = true
+    }
+
+    // 繪製文字的畫筆
+    private val textPaint = Paint().apply {
+        color = Color.WHITE
+        textSize = 20f
+        isAntiAlias = true
+        textAlign = Paint.Align.CENTER
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // 繪製轉盤
+        val centerX = width / 2f
+        val centerY = height / 2f
+        canvas.drawCircle(centerX, centerY, wheelRadius, wheelPaint)
+
+        // 繪製轉盤上的選項
+        val optionAngle = 360f / options.size
+        for (i in options.indices) {
+            val startAngle = i * optionAngle + currentAngle
+            val path = Path()
+            path.addArc(
+                centerX - wheelRadius,
+                centerY - wheelRadius,
+                centerX + wheelRadius,
+                centerY + wheelRadius,
+                startAngle,
+                optionAngle
+            )
+            canvas.drawTextOnPath(options[i], path, 0f, 0f, textPaint)
+        }
+
+        // 繪製指針
+        canvas.drawCircle(centerX, centerY, pointerRadius, pointerPaint)
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // 在這裡我們將處理轉盤的觸摸事件
+        return super.onTouchEvent(event)
+    }
+}
+
